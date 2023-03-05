6bd6d74706bf94db93ee293ccc85d2b2fea741af
Arihantjain0666 <126955193+Arihantjain0666@users.noreply.github.com>
2023-03-05 16:26:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+var trex ,trex_running;
+function preload(){
+  trex_running = loadAnimation("trex1.png" , "trex3.png" , "trex4.png");
+  
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+ trex =createSprite(50,150,20,50);
+ trex.addAnimation ("trex_running", trex_running);
+trex.scale=0.5;
+//create a ground sprite
+ground = createSprite(300,180,600,20);
+
+
+
+
+
+
+
+
+
+}
+
+function draw(){
+  background("lightblue")
+  drawSprites();
+
+}
